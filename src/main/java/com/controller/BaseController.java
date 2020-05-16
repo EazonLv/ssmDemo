be9b18ea31911fd375859f825095319436d29839
EazonLv <799525811@qq.com>
2020-05-16 19:18:01 +0800

    增加log4j日志

--- src/main/java/com/controller/BaseController.java
+++ src/main/java/com/controller/BaseController.java
@@ -1,6 +1,8 @@
 package com.controller;
 
 import com.service.UserService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -11,6 +13,12 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 public class BaseController {
+
+
+	/**
+	 * 日志
+	 */
+	protected final Log log = LogFactory.getLog(getClass());
 
 	/**
 	 * 公用方法：获取Request
